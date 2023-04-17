a7f05c9bf0a9b51c2646e0742544c697179a71bd
FatimaG04 <123663602+FatimaG04@users.noreply.github.com>
2023-04-17 10:59:24 -0600

    Update AgregarQuiz.jsx

--- src/components/AgregarQuiz.jsx
+++ src/components/AgregarQuiz.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import {useState} from "react";
 import '../assets/styles/AgregarQuiz.css'
-import { Button } from "bootstrap";
+import { button } from "bootstrap";
 
 function AgregarQuiz (){
     const [preguntas, setPreguntas] = useState ([""])
 
    return (
     <div>
-     Agregar preguntas: 
-
+     Preguntas:
+     <button
+        onClick={() => {
+          setPreguntas([...preguntas, ''])
+        }}
+      >
+        Agregar Respuestas: 
+      </button>
      {
         preguntas.map ((item) => {
-            return <input value={item} />
+            return <input value={item}/>
             })
      }
     </div>
